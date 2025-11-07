2e6953bf7853abbeb7e65d9f9215691946f6881d
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2025-11-07 03:58:19 +0300

    Add getter for tool-specific sources

--- skelet/__init__.py
+++ skelet/__init__.py
@@ -8,3 +8,4 @@ from skelet.sources.json import JSONSource as JSONSource  # noqa: F401
 from skelet.sources.yaml import YAMLSource as YAMLSource  # noqa: F401
 from skelet.sources.env import EnvSource as EnvSource  # noqa: F401
 from skelet.sources.memory import MemorySource as MemorySource  # noqa: F401
+from skelet.sources.getter_for_libraries import for_tool as for_tool  # noqa: F401
---
+++ skelet/sources/getter_for_libraries.py
@@ -0,0 +1,13 @@
+from typing import List
+
+from skelet.sources.abstract import AbstractSource
+from skelet import EnvSource, TOMLSource, JSONSource, YAMLSource
+
+
+def for_tool(tool_name: str) -> List[AbstractSource]:
+    return (
+        EnvSource.for_library(tool_name)
+        + TOMLSource.for_library(tool_name)
+        + YAMLSource.for_library(tool_name)
+        + JSONSource.for_library(tool_name)
+    )
